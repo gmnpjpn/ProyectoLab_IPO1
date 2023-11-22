e5223eaeb99af00ad6090d756a751cfbf038be72
German Pajarero <germanpajareropino@gmail.com>
2023-11-22 23:11:23 +0100

    Modificaciones en el proyecto

--- ProyectoIPO_Lab2324/LandingWindow.xaml.cs
+++ ProyectoIPO_Lab2324/LandingWindow.xaml.cs
@@ -19,9 +19,39 @@ namespace ProyectoIPO_Lab2324
     /// </summary>
     public partial class LandingWindow : Window
     {
-        public LandingWindow(TextBox textbox_user, TextBox textbox_password)
+
+        private string textbox_user_local;
+        private string dateTime_local;
+
+        public LandingWindow(String textbox_user, String dateTime)
         {
             InitializeComponent();
+            textbox_user_local = textbox_user;
+            dateTime_local = dateTime;
+
+            textblock_lastTime.Text = "Ultimo Acceso: " + dateTime;
+            textblock_username.Text = textbox_user_local;
+        }
+
+        // It makes program to shutdown when clicked (main window is hide and not closed to preserve execution running)
+        private void closingButton_event(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
+           
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            LandingWindow landingWindow = new LandingWindow(textbox_user_local, dateTime_local);
+            landingWindow.Show();
+            this.Close();
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Close();
         }
     }
 }
