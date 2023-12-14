d4fa63d380f992fd74a340f1ab0ae4ed2605ec6a
Benetd <Benedicto.Tapetado@alu.uclm.es>
2023-12-14 10:13:34 +0100

    Internacionalización LoginWindow
    Permite elegir idioma entre inglés o español

--- ProyectoIPO_Lab2324/App.xaml.cs
+++ ProyectoIPO_Lab2324/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -20,5 +22,37 @@ namespace ProyectoIPO_Lab2324
             WindowManager.LoginWindowInstance = loginWindow;
         }
 
+        public static ResourceDictionary SelectCulture(string idioma)
+        {
+            var resourceDictionary = new ResourceDictionary();
+            switch (idioma)
+            {
+                case "en-US":
+                    resourceDictionary.Source =
+                    new Uri("/Resources/StringResources/StringResources.en-US.xaml", UriKind.Relative);
+                    break;
+                case "es-ES":
+                    resourceDictionary.Source =
+                    new Uri("/Resources/StringResources/StringResources.es-ES.xaml", UriKind.Relative);
+                    break;
+                default:
+                    resourceDictionary.Source =
+                    new Uri("/Resources/StringResources/StringResources.es-ES.xaml", UriKind.Relative);
+                    break;
+            }
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(idioma);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(idioma);
+            return resourceDictionary;
+        }
+
+
     }
 }
+
+
+
+
+
+
+
+
