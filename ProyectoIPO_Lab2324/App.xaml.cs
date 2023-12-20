798419847f1c287d7eb983df2503f9f4e137f042
German Pajarero <germanpajareropino@gmail.com>
2023-12-20 22:13:31 +0100

    Creados diccionarios

--- ProyectoIPO_Lab2324/App.xaml.cs
+++ ProyectoIPO_Lab2324/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace ProyectoIPO_Lab2324
 {
@@ -18,6 +21,29 @@ namespace ProyectoIPO_Lab2324
             base.OnStartup(e);
             LoginWindow loginWindow = new LoginWindow();
             WindowManager.LoginWindowInstance = loginWindow;
+        }
+
+        public static ResourceDictionary SelectCulture(string idioma)
+        {
+            var resourceDictionary = new ResourceDictionary();
+            switch (idioma)
+            {
+                case "en-US":
+                    resourceDictionary.Source =
+                    new Uri("/resources/StringResources.en-US.xaml", UriKind.Relative);
+                    break;
+                case "es-ES":
+                    resourceDictionary.Source =
+                    new Uri("/resources/StringResources.es-ES.xaml", UriKind.Relative);
+                    break;
+                default:
+                    resourceDictionary.Source =
+                    new Uri("/resources/StringResources.es-ES.xaml", UriKind.Relative);
+                    break;
+            }
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(idioma);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(idioma);
+            return resourceDictionary;
         }
 
     }
