b657d0c81c97e81128f97ebf932d7a6771af6da2
Benetd <Benedicto.Tapetado@alu.uclm.es>
2024-01-12 14:15:39 +0100

    Cambios en ShoppinCart, Contact y FAQs

--- ProyectoIPO_Lab2324/LandingWindow.xaml.cs
+++ ProyectoIPO_Lab2324/LandingWindow.xaml.cs
@@ -339,7 +339,26 @@ namespace ProyectoIPO_Lab2324
 
         private void btnBuyAlbum_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Funcion no implementada todavía");
+            
+            if (lstAlbumList.SelectedItem != null && lstAlbumList.SelectedItem is Album selectedAlbum)
+            {
+                
+                bool existsInShoppingCart = GlobalData.ShoppingCartList.Any(a =>
+                    a.Name == selectedAlbum.Name &&
+                    a.Author == selectedAlbum.Author
+                );
+
+                if (!existsInShoppingCart)
+                {
+                    GlobalData.ShoppingCartList.Add(selectedAlbum);
+                    MessageBox.Show("Añadido correctamente al carrito de compra.", "Añadido al carrito");
+                  
+                }
+                else
+                {
+                    MessageBox.Show("El álbum ya había sido añadido al carrito.", "Error al añadir al carrito");
+                }
+            }
         }
 
         private void stopAtClose(object sender, System.ComponentModel.CancelEventArgs e)
--- ProyectoIPO_Lab2324/ShoppingCartWindow.xaml.cs
+++ ProyectoIPO_Lab2324/ShoppingCartWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,11 @@ namespace ProyectoIPO_Lab2324
 
             textblock_username.Text = GlobalData.Username;
             textblock_lastTime.Text = GlobalData.CurrentDateTime;
+
+            listbox_shopping_cart.ItemsSource = GlobalData.ShoppingCartList;
+            listbox_shopping_cart.DisplayMemberPath = "Name";
+
+           // add();
         }
         private void btnHome_Click(object sender, RoutedEventArgs e)
         {
@@ -65,5 +71,51 @@ namespace ProyectoIPO_Lab2324
         {
             System.Windows.Application.Current.Shutdown();
         }
+
+        private void buy_button_Click(object sender, RoutedEventArgs e)
+        {
+            // Crear una instancia de la ventana para agregar un álbum
+            PurchaseFormWindow purchaseFormWindow = new PurchaseFormWindow();
+            // Mostrar la ventana como un diálogo modal
+            bool? result = purchaseFormWindow.ShowDialog();
+
+            GlobalData.ShoppingCartList.Clear();
+            listbox_shopping_cart.ItemsSource = GlobalData.ShoppingCartList;
+            listbox_shopping_cart.DisplayMemberPath = "Name";
+
+            ShoppingCartWindow shoppingCart = new ShoppingCartWindow();
+            WindowManager.ShoppingCartWindowInstance = shoppingCart;
+            this.Hide();
+            shoppingCart.Show();
+
+        }
+
+        private void combobox_language_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem cbi = (ComboBoxItem)combobox_language.SelectedItem; string selectedText = cbi.Content.ToString();
+            if ((selectedText.Equals("ES") || selectedText.Equals("SP")) && !CultureInfo.CurrentCulture.Name.Equals("es-ES"))
+            {
+                Resources.MergedDictionaries.Add(App.SelectCulture("es-ES"));
+            }
+            else if (selectedText.Equals("EN")
+            && !CultureInfo.CurrentCulture.Name.Equals("en-US"))
+            {
+                Resources.MergedDictionaries.Add(App.SelectCulture("en-US"));
+            }
+        }
+
+        /* private void add()
+         {
+             double add = 0;
+             Album album = new Album();
+             for (int i = 0; i < GlobalData.ShoppingCartList.Count; i++)
+             {
+                 //album = listbox_shopping_cart.Items.GetItemAt(i);
+                 add = Convert.ToDouble(listbox_shopping_cart.Items.GetItemAt(i));
+                 add += add;
+             }
+             tbPvp.Text = add.ToString(Name);
+         }
+        */
     }
 }
